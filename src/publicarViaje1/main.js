85985edad5e7a566b22ab8a5e0214ca19b2e10d8
Ghost-ofc <juan089207@gmail.com>
2024-07-04 09:30:02 -0500

    Cambios juan

--- src/publicarViaje1/main.js
+++ src/publicarViaje1/main.js
@@ -1,62 +1,89 @@
-const express = require('express');
 const AWS = require('aws-sdk');
-const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
 
 // Configuramos AWS DynamoDB
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-// Configuramos la aplicación Express
-const app = express();
-app.use(bodyParser.json());
+exports.handler = async (event) => {
+    console.log('Event:', JSON.stringify(event));
 
-// Endpoint para publicar un viaje
-app.post('/publicar-viaje', async (req, res) => {
-    const {
-        direccionorigen, ciudadorigen, provinciaorigen, departamentoorigen, distritoorigen,
-        departamentodestino, ciudaddestino, provinciadestino, distritodestino, direcciondestino
-    } = req.body;
+    const token = event.headers.Authorization.replace("Bearer ", "");
 
-    // Generamos IDs únicos para cada registro
-    const idViajeDestino = uuidv4();
-    const idViajeOrigen = uuidv4();
-
-    // Preparamos los datos para DynamoDB
-    const destinoParams = {
-        TableName: "ViajeDestino",
-        Item: {
-            idviajedestino: idViajeDestino,
-            ciudaddestino,
-            departamentodestino,
-            direcciondestino,
-            provinciadestino,
-            distritodestino
-        }
-    };
-
-    const origenParams = {
-        TableName: "ViajeOrigen",
-        Item: {
-            idviajeorigen: idViajeOrigen,
-            ciudadorigen,
-            departamentoorigen,
-            direccionorigen,
-            provinciaorigen,
-            distritoorigen
-        }
-    };
+    let userId;
+    try {
+        const decoded = jwt.decode(token);
+        userId = decoded.userId.toString(); 
+    } catch (err) {
+        console.error('Error decoding token:', err);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'Invalid token' })
+        };
+    }
 
     try {
-        // Insertamos los datos en las tablas de DynamoDB
+        const {
+            direccionorigen, ciudadorigen, provinciaorigen, departamentoorigen, distritoorigen,
+            departamentodestino, ciudaddestino, provinciadestino, distritodestino, direcciondestino
+        } = JSON.parse(event.body);
+
+        const idViajeDestino = uuidv4();
+        const idViajeOrigen = uuidv4();
+
+        const destinoParams = {
+            TableName: process.env.VIAJE_DESTINO_TABLE,
+            Item: {
+                idviajedestino: idViajeDestino,
+                ciudaddestino,
+                departamentodestino,
+                direcciondestino,
+                provinciadestino,
+                distritodestino
+            }
+        };
+
+        const origenParams = {
+            TableName: process.env.VIAJE_ORIGEN_TABLE,
+            Item: {
+                idviajeorigen: idViajeOrigen,
+                ciudadorigen,
+                departamentoorigen,
+                direccionorigen,
+                provinciaorigen,
+                distritoorigen
+            }
+        };
+
+        const viajeParams = {
+            TableName: process.env.VIAJES_TABLE,
+            Item: {
+                id_viajes: uuidv4(),
+                conductorId: userId,
+                disponible: true,
+                asientosReservados: 0,
+                viajeOrigen: idViajeOrigen,
+                viajeDestino: idViajeDestino
+            }
+        };
+
         await dynamodb.put(destinoParams).promise();
         await dynamodb.put(origenParams).promise();
-        res.status(201).send({
-            message: "Viaje publicado correctamente",
-            idViajeOrigen,
-            idViajeDestino
-        });
+        await dynamodb.put(viajeParams).promise();
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({
+                message: "Viaje publicado correctamente",
+                idViajeOrigen,
+                idViajeDestino
+            }),
+        };
     } catch (error) {
-        console.error("Error al publicar el viaje:", error);
-        res.status(500).send({ message: "Error al publicar el viaje", error: error.message });
+        console.error('Error al publicar el viaje:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error al publicar el viaje', error: error.message }),
+        };
     }
-});
+};
